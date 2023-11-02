e2b78dbd8df4891d6b1d867640e03978923b0af9
Dumindu-Bandara <dumindubandara1999@gmail.com>
2023-11-02 11:11:43 +0530

    fixed bug on genvar

--- rtl/mat_mul_simd.sv
+++ rtl/mat_mul_simd.sv
@@ -16,8 +16,9 @@ localparam DEPTH = $clog2(N); // number of summation stages
 logic signed [W_OUT-1:0] partial_sum [N][N][DEPTH+1][N][SIMD_WIDTH]; // SIMD for the intermediate values
 logic valid_buffer[DEPTH+1]; // holds the valid signal for each summation stage
 
-genvar m1_row_num, m2_col_num, d0_item_num, depth, item_num;
+
 generate
+genvar m1_row_num, m2_col_num, d0_item_num, depth, item_num;
 for (m1_row_num = 0; m1_row_num < N; m1_row_num = m1_row_num + 1) begin: m1_row_iter
     for (m2_col_num = 0; m2_col_num < N; m2_col_num++) begin: m2_col_iter
 
@@ -25,18 +26,18 @@ for (m1_row_num = 0; m1_row_num < N; m1_row_num = m1_row_num + 1) begin: m1_row_
         for (d0_item_num = 0; d0_item_num < N; d0_item_num = d0_item_num + SIMD_WIDTH) begin: d0_item_iter
             always_comb begin
                 // Perform SIMD multiplications if valid_in is high
+
                 if (valid_in) begin
-                    for (genvar simd_idx = 0; simd_idx < SIMD_WIDTH; simd_idx = simd_idx + 1) begin: simd_mul_iter
-                        partial_sum[m1_row_num][m2_col_num][0][d0_item_num + simd_idx][simd_idx] =
-                            $signed(matrix_1[m1_row_num][d0_item_num + simd_idx]) *
-                            $signed(matrix_2[d0_item_num + simd_idx][m2_col_num]);
+                    for (int simd_idx = 0; simd_idx < SIMD_WIDTH; simd_idx = simd_idx + 1) begin: simd_mul_iter
+                        partial_sum[m1_row_num][m2_col_num][0][d0_item_num + simd_idx][simd_idx] = $signed(matrix_1[m1_row_num][d0_item_num + simd_idx]) * $signed(matrix_2[d0_item_num + simd_idx][m2_col_num]);
                     end
                 end else begin
-                    for (genvar simd_idx = 0; simd_idx < SIMD_WIDTH; simd_idx = simd_idx + 1) begin: simd_zero_iter
+                    for (int simd_idx = 0; simd_idx < SIMD_WIDTH; simd_idx = simd_idx + 1) begin: simd_zero_iter
                         partial_sum[m1_row_num][m2_col_num][0][d0_item_num + simd_idx][simd_idx] = 0;
                     end
                 end
                 valid_buffer[0] = valid_in;
+                
             end
         end
         
@@ -45,10 +46,8 @@ for (m1_row_num = 0; m1_row_num < N; m1_row_num = m1_row_num + 1) begin: m1_row_
             for (item_num = 0; item_num < N/2**(depth+1); item_num++) begin: item_iter
                 always_ff @(posedge clk) begin
                   if (cen) begin
-                    for (genvar simd_idx = 0; simd_idx < SIMD_WIDTH; simd_idx = simd_idx + 1) begin: simd_sum_iter
-                        partial_sum[m1_row_num][m2_col_num][depth+1][item_num*SIMD_WIDTH + simd_idx][simd_idx] <=
-                            partial_sum[m1_row_num][m2_col_num][depth][2*item_num*SIMD_WIDTH + simd_idx] +
-                            partial_sum[m1_row_num][m2_col_num][depth][2*item_num*SIMD_WIDTH + simd_idx + 1];
+                    for (int simd_idx = 0; simd_idx < SIMD_WIDTH; simd_idx = simd_idx + 1) begin: simd_sum_iter
+                        partial_sum[m1_row_num][m2_col_num][depth+1][item_num*SIMD_WIDTH + simd_idx][simd_idx] <= partial_sum[m1_row_num][m2_col_num][depth][2*item_num*SIMD_WIDTH + simd_idx][simd_idx] + partial_sum[m1_row_num][m2_col_num][depth][2*item_num*SIMD_WIDTH + simd_idx + 1][simd_idx];
                     end
                     valid_buffer[depth+1] <= valid_buffer[depth];
                   end
