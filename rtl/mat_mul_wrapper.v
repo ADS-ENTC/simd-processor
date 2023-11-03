b4fcacecdc4c2c117c43f3c39bebed8a06d2539a
SupunDK <supun2110@gmail.com>
2023-11-03 13:26:48 +0530

    converted packed arrays into a word

--- rtl/mat_mul_wrapper.v
+++ rtl/mat_mul_wrapper.v
@@ -4,11 +4,11 @@ module mat_mul_wrapper#(
     parameter N = 8
 )(
     input wire clk, cen, valid_in, mode,
-    input wire signed [N-1:0][N-1:0][W_IN-1:0] matrix_1, 
-    input reg signed [N-1:0][N-1:0][W_IN-1:0] matrix_2,
+    input wire signed [N*N*W_IN-1:0] matrix_1, 
+    input wire signed [N*N*W_IN-1:0] matrix_2,
     output reg valid_out,
-    output reg signed [N-1:0][N-1:0][W_OUT-1:0] result
-)
+    output reg signed [N*N*W_OUT-1:0] result
+);
 
 mat_mul #(.W_IN(W_IN), .W_OUT(W_OUT), .N(N)) mm (.clk(clk), .cen(cen), .valid_in(valid_in), .mode(mode), .matrix_1(matrix_1), .matrix_2(matrix_2), .valid_out(valid_out), .result(result));
 
