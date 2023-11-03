a43254f73dd9dd9688a4d02bdb300e01d33a4927
SupunDK <supun2110@gmail.com>
2023-11-03 15:49:46 +0530

    reverted back to just multiplication functionality

--- rtl/mat_mul.sv
+++ rtl/mat_mul.sv
@@ -3,17 +3,16 @@ module mat_mul #(
     parameter W_OUT = 32,
     parameter N = 2 // N*N matrix
 )(
-    input logic clk, cen, valid_in, mode,
-    input logic signed [N-1:0][N-1:0][W_IN-1:0] matrix_1, 
-    input logic signed [N-1:0][N-1:0][W_IN-1:0] matrix_2,
+    input logic clk, cen, valid_in,
+    input logic signed [N-1:0][N-1:0][W_IN-1:0] matrix_1, matrix_2,
     output logic valid_out,
     output logic signed [N-1:0][N-1:0][W_OUT-1:0] result
 );
 
-localparam DEPTH = $clog2(N); // number of summation stages for multiplication
+localparam DEPTH = $clog2(N); // number of summation stages
 
-logic signed [W_OUT-1:0] partial_sum [N][N][DEPTH+1][N]; // holds the intermediate values of the summation stages in multiplication
-logic valid_buffer[DEPTH+1]; // holds the valid signal for each summation stage for multiplication 
+logic signed [N-1:0][N-1:0][DEPTH:0][N-1:0][W_OUT-1:0] partial_sum; // holds the intermediate values of the summation stages
+logic valid_buffer[DEPTH+1]; // holds the valid signal for each summation stage
 
 genvar m1_row_num, m2_col_num, d0_item_num, depth, item_num;
 generate
@@ -29,8 +28,7 @@ for (m1_row_num = 0; m1_row_num < N; m1_row_num = m1_row_num + 1) begin: m1_row_
                 else
                     partial_sum[m1_row_num][m2_col_num][0][d0_item_num] = 0;
                 
-                // if the module is in multiplication mode buffer the valid signal
-                valid_buffer[0] = valid_in & (~mode);
+                valid_buffer[0] = valid_in;
             end
         end
         
@@ -38,7 +36,7 @@ for (m1_row_num = 0; m1_row_num < N; m1_row_num = m1_row_num + 1) begin: m1_row_
         for (depth = 0; depth < DEPTH; depth++) begin: depth_iter
             for (item_num = 0; item_num < N/2**(depth+1); item_num++) begin: item_iter
                 always_ff @(posedge clk) begin
-                  if (cen && mode == 0'b0) begin
+                  if (cen) begin
                     partial_sum[m1_row_num][m2_col_num][depth+1][item_num] <= partial_sum[m1_row_num][m2_col_num][depth][2*item_num] + partial_sum[m1_row_num][m2_col_num][depth][2*item_num+1];
                     valid_buffer[depth+1] <= valid_buffer[depth];
                   end
@@ -46,20 +44,9 @@ for (m1_row_num = 0; m1_row_num < N; m1_row_num = m1_row_num + 1) begin: m1_row_
             end
         end
 
-        // assign the result
-        always_comb begin
-            if (mode == 0'b0)
-                result[m1_row_num][m2_col_num] = partial_sum[m1_row_num][m2_col_num][DEPTH][0];
-            else
-                result[m1_row_num][m2_col_num] = $signed(matrix_1[m1_row_num][m2_col_num]) + $signed(matrix_2[m1_row_num][m2_col_num]);
-        end
+        assign result[m1_row_num][m2_col_num] = partial_sum[m1_row_num][m2_col_num][DEPTH][0];
+        assign valid_out = valid_buffer[DEPTH];
     end
 end
 endgenerate
-
-always_comb begin
-    valid_out = (mode == 0'b0) ? valid_buffer[DEPTH] : valid_in;
-end
-
-
 endmodule
--- tb/mat_mul_tb.sv
+++ tb/mat_mul_tb.sv
@@ -7,11 +7,9 @@ module mat_mul_tb;
 
     localparam delay = $clog2(N) + 1;
 
-    logic clk, cen, valid_in, valid_out, mode;
-    logic signed [N-1:0][N-1:0][W_IN-1:0] matrix_1;
-    logic signed [N-1:0][N-1:0][W_IN-1:0] matrix_2;
-    logic signed [N-1:0][N-1:0][W_OUT-1:0] result;
-    logic signed [N-1:0][N-1:0][W_OUT-1:0] exp_result;
+    logic clk, cen, valid_in, valid_out;
+    logic signed [N-1:0][N-1:0][W_IN-1:0] matrix_1, matrix_2;
+    logic signed [N-1:0][N-1:0][W_OUT-1:0] result, exp_result;
 
     mat_mul #(.W_IN(W_IN), .W_OUT(W_OUT), .N(N)) dut (.*);
 
@@ -24,16 +22,9 @@ module mat_mul_tb;
     // simulating the device functionality
     initial begin
         $srandom(56);
-
-        // testing for multiplication
-        $display("Testing the multiplication");
-
         cen = 1;
-        mode = 0;
+        valid_in = 1;
         repeat (10) begin
-            $display("========== Test Case ==========");
-            valid_in = $urandom_range(0, 1);
-
             repeat (2) @(posedge clk);
             
             // giving the inputs
@@ -61,149 +52,10 @@ module mat_mul_tb;
             end
 
             // checking the accuracy of the result
-            assert (valid_in == valid_out)
-                $display("OK Valid: %d", valid_out);
+            assert (result == exp_result) 
+                $display("OK");
             else
-                $error("Valid signals do not match: valid_in = %d, valid_out = %d", valid_in, valid_out);
-
-            if (valid_out == 1) begin
-                assert (result == exp_result) 
-                    $display("OK Result");
-                else
-                    $error("Output does not match: result = %p, exp_result = %p", result, exp_result);
-            end
-        end
-
-        // testing for addition
-        $display("Testing the addition");
-
-        cen = 1;
-        mode = 1;
-        repeat (10) begin
-            $display("========== Test Case ==========");
-            valid_in = $urandom_range(0, 1);
-
-            // giving the inputs
-            @(posedge clk);
-            #1
-            for (int i=0; i<N; i++) begin
-                for (int j=0; j<N; j++) begin
-                    matrix_1[i][j] = $urandom_range(0, 2**W_IN-1);
-                    matrix_2[i][j] = $urandom_range(0, 2**W_IN-1);
-                end
-            end
-
-            // calculating the expected result
-            for (int i=0; i<N; i++) begin
-                for (int j=0; j<N; j++) begin
-                    exp_result[i][j] = $signed(matrix_1[i][j]) + $signed(matrix_2[i][j]);
-                end
-            end
-
-            // checking the accuracy of the result
-            @(posedge clk);
-            #1
-
-            assert (valid_in == valid_out)
-                $display("OK Valid: %d", valid_out);
-            else
-                $error("Valid signals do not match: valid_in = %d, valid_out = %d", valid_in, valid_out);
-
-            if (valid_out == 1) begin
-                assert (result == exp_result) 
-                    $display("OK Result");
-                else
-                    $error("Output does not match: result = %p, exp_result = %p", result, exp_result);
-            end
-        end
-
-        // testing for multiplication and addition intermitently
-        $display("Testing the multiplication and addition intermitently");
-
-        cen = 1;
-
-        repeat (10) begin
-            $display("========== Test Case ==========");
-            
-            // Multiplication
-            mode = 0;
-            valid_in = $urandom_range(0, 1);
-
-            repeat (2) @(posedge clk);
-            
-            // giving the inputs
-            @(posedge clk);
-            #1
-            for (int i=0; i<N; i++) begin
-                for (int j=0; j<N; j++) begin
-                    matrix_1[i][j] = $urandom_range(0, 2**W_IN-1);
-                    matrix_2[i][j] = $urandom_range(0, 2**W_IN-1);
-                end
-            end
-
-            // waiting for the latency
-            repeat (delay) @(posedge clk);
-            #1
-
-            // calculating the expected result
-            for (int i=0; i<N; i++) begin : matrix_1_row_iter
-                for (int j=0; j<N; j++) begin : matrix_2_column_iter
-                    exp_result[i][j] = 0;
-                    for (int k=0; k<N; k++) begin : item_iter
-                        exp_result[i][j] = $signed(exp_result[i][j]) + $signed(matrix_1[i][k]) * $signed(matrix_2[k][j]);
-                    end
-                end
-            end
-
-            // checking the accuracy of the result
-            assert (valid_in == valid_out)
-                $display("OK Valid: %d [Multiplication]", valid_out);
-            else
-                $error("Valid signals do not match: valid_in = %d, valid_out = %d [Multiplication]", valid_in, valid_out);
-
-            if (valid_out == 1) begin
-                assert (result == exp_result) 
-                    $display("OK Result [Multiplication]");
-                else
-                    $error("Output does not match: result = %p, exp_result = %p [Multiplication]", result, exp_result);
-            end      
-
-            // Addition 
-            mode = 1;
-            valid_in = $urandom_range(0, 1);
-
-            // giving the inputs
-            @(posedge clk);
-            #1
-            for (int i=0; i<N; i++) begin
-                for (int j=0; j<N; j++) begin
-                    matrix_1[i][j] = $urandom_range(0, 2**W_IN-1);
-                    matrix_2[i][j] = $urandom_range(0, 2**W_IN-1);
-                end
-            end
-
-            // calculating the expected result
-            for (int i=0; i<N; i++) begin
-                for (int j=0; j<N; j++) begin
-                    exp_result[i][j] = $signed(matrix_1[i][j]) + $signed(matrix_2[i][j]);
-                end
-            end
-
-            // checking the accuracy of the result
-            @(posedge clk);
-            #1
-
-            assert (valid_in == valid_out)
-                $display("OK Valid: %d [Addition]", valid_out);
-            else
-                $error("Valid signals do not match: valid_in = %d, valid_out = %d [Addition]", valid_in, valid_out);
-
-            if (valid_out == 1) begin
-                assert (result == exp_result) 
-                    $display("OK Result [Addition]");
-                else
-                    $error("Output does not match: result = %p, exp_result = %p [Addition]", result, exp_result);
-            end      
+                $error("Output does not match: result = %p, exp_result = %p", result, exp_result);
         end
         
         $stop;
