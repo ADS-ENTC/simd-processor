495dec0703ca0bd4b12f0240b3d18fa8ae2910c0
SupunDK <supun2110@gmail.com>
2023-11-04 16:45:45 +0530

    made one data_in port

--- rtl/mat_mul_wrapper.v
+++ rtl/mat_mul_wrapper.v
@@ -4,12 +4,11 @@ module mat_mul_wrapper#(
     parameter N = 8
 )(
     input wire clk, resetn, valid_in,
-    input wire signed [N*N*W_IN-1:0] matrix_1, 
-    input wire signed [N*N*W_IN-1:0] matrix_2,
+    input wire signed [2*N*N*W_IN-1:0] data_in, 
     output reg valid_out,
     output reg signed [N*N*W_OUT-1:0] result
 );
 
-mat_mul #(.W_IN(W_IN), .W_OUT(W_OUT), .N(N)) mm (.clk(clk), .resetn(resetn), .valid_in(valid_in), .matrix_1(matrix_1), .matrix_2(matrix_2), .valid_out(valid_out), .result(result));
+mat_mul #(.W_IN(W_IN), .W_OUT(W_OUT), .N(N)) mm (.clk(clk), .resetn(resetn), .valid_in(valid_in), .matrix_1(data_in[N*N*W_IN-1:0]), .matrix_2(data_in[2*N*N*W_IN-1:N*N*W_IN]), .valid_out(valid_out), .result(result));
 
 endmodule
