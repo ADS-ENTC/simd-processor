dba00fd79e02ffdafcb46f5e0eca5234b6110a10
dakshinatharindu <tharinduokd@gmail.com>
2023-11-04 19:09:10 +0530

    change valid_out reg to wire

--- rtl/mat_mul_wrapper.v
+++ rtl/mat_mul_wrapper.v
@@ -1,12 +1,12 @@
 module mat_mul_wrapper#(
     parameter W_IN = 8,
     parameter W_OUT = 32,
-    parameter N = 8
+    parameter N = 4
 )(
     input wire clk, resetn, valid_in,
     input wire signed [2*N*N*W_IN-1:0] data_in, 
-    output reg valid_out,
-    output reg signed [N*N*W_OUT-1:0] result
+    output wire valid_out,
+    output wire signed [N*N*W_OUT-1:0] result
 );
 
 mat_mul #(.W_IN(W_IN), .W_OUT(W_OUT), .N(N)) mm (.clk(clk), .resetn(resetn), .valid_in(valid_in), .matrix_1(data_in[N*N*W_IN-1:0]), .matrix_2(data_in[2*N*N*W_IN-1:N*N*W_IN]), .valid_out(valid_out), .result(result));
