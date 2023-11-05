413e840ba3978dae17c0a976e4bb43257355530d
SupunDK <supun2110@gmail.com>
2023-11-05 09:52:42 +0530

    changed the output port type

--- rtl/mat_mul_wrapper.v
+++ rtl/mat_mul_wrapper.v
@@ -5,8 +5,8 @@ module mat_mul_wrapper#(
 )(
     input wire clk, resetn, valid_in,
     input wire signed [2*N*N*W_IN-1:0] data_in, 
-    output reg valid_out,
-    output reg signed [N*N*W_OUT-1:0] result
+    output wire valid_out,
+    output wire signed [N*N*W_OUT-1:0] result
 );
 
 mat_mul #(.W_IN(W_IN), .W_OUT(W_OUT), .N(N)) mm (.clk(clk), .resetn(resetn), .valid_in(valid_in), .matrix_1(data_in[N*N*W_IN-1:0]), .matrix_2(data_in[2*N*N*W_IN-1:N*N*W_IN]), .valid_out(valid_out), .result(result));
