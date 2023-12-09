2dad203374402a7b23362f12668a0bf5f7d31741
SupunDK <supun2110@gmail.com>
2023-12-10 02:09:52 +0530

    fix issues with memory read

--- rtl/pe_fetch_unit.sv
+++ rtl/pe_fetch_unit.sv
@@ -5,36 +5,36 @@ module pe_fetch_unit #(
     parameter PC_LEN = 12,
     parameter OPCODE_LEN = 4,
     parameter PE_OPCODE_LEN = 4,
-    parameter DRAM_DEPTH = 512
+    parameter DRAM_DEPTH = 256,
+    localparam DRAM_ADDR_WIDTH = $clog2(DRAM_DEPTH)
 )(
-    input logic rstn, clk, pe_stage_1_valid, pe_stage_2_valid, store_result, valid, stop,
+    input logic rstn, clk, pe_stage_1_valid, pe_stage_2_valid, store_result, valid,
     input logic [PE_ELEMENTS-1:0][DATA_LEN-1:0] pe_stage_1_output,
     input logic [DATA_LEN-1:0] pe_stage_2_output,
 
     output logic [PE_OPCODE_LEN-1:0] pe_opcode,
     output logic [PE_ELEMENTS-1:0][DATA_LEN-1:0] data_a, data_b,
 
-    input [INST_LEN-1:0]inst_read_data,
-    output [PC_LEN-1:0]inst_read_addr,  
+    input logic [INST_LEN-1:0]inst_read_data,
+    output logic [PC_LEN-1:0]inst_read_addr,  
 
-    input [PE_ELEMENTS-1:0][DATA_LEN-1:0]ram_a_read_data,
-    output [DRAM_ADDR_WIDTH-1:0]ram_a_read_addr, 
+    input logic [PE_ELEMENTS-1:0][DATA_LEN-1:0]ram_a_read_data,
+    output logic [DRAM_ADDR_WIDTH-1:0]ram_a_read_addr, 
+    output logic ram_a_rd_en,
 
-    input [PE_ELEMENTS-1:0][DATA_LEN-1:0]ram_b_read_data,
-    output [DRAM_ADDR_WIDTH-1:0]ram_b_read_addr, 
+    input logic [PE_ELEMENTS-1:0][DATA_LEN-1:0]ram_b_read_data,
+    output logic [DRAM_ADDR_WIDTH-1:0]ram_b_read_addr, 
+    output logic ram_b_rd_en,
 
-    output [DRAM_ADDR_WIDTH-1:0]ram_result_write_addr, 
-    output [PE_ELEMENTS-1:0][DATA_LEN-1:0]ram_result_write_data,
-    output ram_result_wr_en  
+    output logic [DRAM_ADDR_WIDTH-1:0]ram_result_write_addr, 
+    output logic [PE_ELEMENTS-1:0][DATA_LEN-1:0]ram_result_write_data,
+    output logic ram_result_wr_en  
 );
 
 typedef enum logic [OPCODE_LEN-1:0] {NOOP, FETCH_A, FETCH_B, ADD, SUB, MUL, DOTP, STORE_TEMP_S1, STORE_TEMP_S2, STORE_RESULT, STOP} OPCODE; 
 
-// local parameters
-localparam DRAM_ADDR_WIDTH = $clog2(DRAM_DEPTH);
-
 // internal signals
-logic save_res_addr, load_a, load_b;
+logic load_a, load_b, save_res_addr;
 logic [DRAM_ADDR_WIDTH-1:0] res_addr;
 logic [INST_LEN-1:0] instruction;
 logic [PC_LEN-1:0] pc;
@@ -52,6 +52,8 @@ assign ram_a_read_addr = data_addr;
 assign ram_b_read_addr = data_addr;
 assign data_a = ram_a_read_data;
 assign data_b = ram_b_read_data;
+assign ram_a_rd_en = load_a;
+assign ram_b_rd_en = load_b;
 
 assign ram_result_wr_en = store_result;
 assign ram_result_write_data = result;
@@ -158,7 +160,11 @@ always_ff@(posedge clk) begin
     if (pe_stage_1_valid && !store_result)
         result <= pe_stage_1_output;
     else if (pe_stage_2_valid_buffer && !store_result) begin
-        {result[DATA_LEN*PE_ELEMENTS-1:DATA_LEN], result[DATA_LEN-1:0]} <= {result[DATA_LEN*PE_ELEMENTS-DATA_LEN-1:0], pe_stage_2_output};
+        result[0] <= pe_stage_2_output;
+
+        for (int i=1; i<PE_ELEMENTS; i++) begin
+            result[i] <= result[i-1];
+        end
     end
 end
 
