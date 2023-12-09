6e5ff5eda16216a0a76496fc3c3f3753123abbd2
dakshinatharindu <tharinduokd@gmail.com>
2023-12-09 11:28:23 +0530

    AXIS master testbench

--- tb/fetch_unit_S00_AXIS_tb.v
+++ tb/fetch_unit_S00_AXIS_tb.v
@@ -1,33 +1,46 @@
 `timescale 1 ns / 1 ps
 
 module fetch_unit_S00_AXIS_tb;
-    parameter integer MATRIX_SIZE	= 4;
-    parameter integer W_IN = 8;
+    parameter BRAM_DEPTH = 10;
+    parameter INSTR_BRAM_DEPTH = 11;
     parameter integer C_S_AXIS_TDATA_WIDTH	= 32;
 
-    wire  [4*2*MATRIX_SIZE*MATRIX_SIZE*W_IN-1 : 0] data_out;
+    wire [BRAM_DEPTH-1:0] mat_a_addr;
+    wire [31:0] mat_a_din;
+    wire mat_a_en;
+    wire [BRAM_DEPTH-1:0] mat_b_addr;
+    wire [31:0] mat_b_din;
+    wire mat_b_en;
+    wire [INSTR_BRAM_DEPTH-1:0] instr_addr;
+    wire [31:0] instr_din;
+    wire instr_en;
+    reg [1:0] bram_sel;
+    reg [31:0] row_width;
+    wire  VALID_FU2PE;
 
-    // AXI4Stream sink: Clock
-    reg S_AXIS_ACLK;
-    // AXI4Stream sink: Reset
-    reg  S_AXIS_ARESETN;
-    // Ready to accept data in
+    reg   S_AXIS_ACLK;
+    reg   S_AXIS_ARESETN;
     wire  S_AXIS_TREADY;
-    // Data in
-    reg [C_S_AXIS_TDATA_WIDTH-1 : 0] S_AXIS_TDATA;
-    // Byte qualifier
-    reg [(C_S_AXIS_TDATA_WIDTH/8)-1 : 0] S_AXIS_TSTRB;
-    // Indicates boundary of last packet
-    reg  S_AXIS_TLAST;
-    // Data is in valid
+    reg  [C_S_AXIS_TDATA_WIDTH-1 : 0] S_AXIS_TDATA;
+    reg  [(C_S_AXIS_TDATA_WIDTH/8)-1 : 0] S_AXIS_TSTRB;
+    reg   S_AXIS_TLAST;
     reg  S_AXIS_TVALID;
 
-    fetch_unit_v1_0_S00_AXIS #(
-        .MATRIX_SIZE(MATRIX_SIZE),
-        .W_IN(W_IN),
-        .C_S_AXIS_TDATA_WIDTH(C_S_AXIS_TDATA_WIDTH)
-    ) fetch_unit_S00_AXIS_inst (
-        .data_out(data_out),
+
+    fetch_unit_v1_0_S00_AXIS dut(
+        .mat_a_addr(mat_a_addr),
+        .mat_a_din(mat_a_din),
+        .mat_a_en(mat_a_en),
+        .mat_b_addr(mat_b_addr),
+        .mat_b_din(mat_b_din),
+        .mat_b_en(mat_b_en),
+        .instr_addr(instr_addr),
+        .instr_din(instr_din),
+        .instr_en(instr_en),
+        .bram_sel(bram_sel),
+        .row_width(row_width),
+        .VALID_FU2PE(VALID_FU2PE),
+
         .S_AXIS_ACLK(S_AXIS_ACLK),
         .S_AXIS_ARESETN(S_AXIS_ARESETN),
         .S_AXIS_TREADY(S_AXIS_TREADY),
@@ -43,18 +56,193 @@ module fetch_unit_S00_AXIS_tb;
     end
 
     initial begin
-        S_AXIS_TSTRB = 15;
         S_AXIS_ARESETN = 0;
         #10 S_AXIS_ARESETN = 1;
-        #20 S_AXIS_TVALID = 1;
+        S_AXIS_TVALID = 0;
+        #50;
 
+        bram_sel = 0;
+        row_width = 2;
+        
         #1;
-        S_AXIS_TDATA = 5;
+        
         @(negedge S_AXIS_ACLK);
-        S_AXIS_TDATA = 8;
+        S_AXIS_TVALID = 1;
+        S_AXIS_TLAST = 0;
+        S_AXIS_TSTRB = 15;
+        S_AXIS_TDATA = 1;
         @(negedge S_AXIS_ACLK);
+        while (S_AXIS_TREADY == 0) @(negedge S_AXIS_ACLK);
         S_AXIS_TDATA = 2;
         @(negedge S_AXIS_ACLK);
+        while (S_AXIS_TREADY == 0) @(negedge S_AXIS_ACLK);
+        S_AXIS_TDATA = 3;
+        @(negedge S_AXIS_ACLK);
+        while (S_AXIS_TREADY == 0) @(negedge S_AXIS_ACLK);
+        S_AXIS_TDATA = 4;
+        @(negedge S_AXIS_ACLK);
+        while (S_AXIS_TREADY == 0) @(negedge S_AXIS_ACLK);
+        S_AXIS_TDATA = 5;
+        @(negedge S_AXIS_ACLK);
+        while (S_AXIS_TREADY == 0) @(negedge S_AXIS_ACLK);
+        S_AXIS_TDATA = 6;
+        @(negedge S_AXIS_ACLK);
+        while (S_AXIS_TREADY == 0) @(negedge S_AXIS_ACLK);
+        S_AXIS_TDATA = 7;
+        @(negedge S_AXIS_ACLK);
+        while (S_AXIS_TREADY == 0) @(negedge S_AXIS_ACLK);
+        S_AXIS_TDATA = 8;
+        @(negedge S_AXIS_ACLK);
+        while (S_AXIS_TREADY == 0) @(negedge S_AXIS_ACLK);
+        S_AXIS_TDATA = 9;
+        @(negedge S_AXIS_ACLK);
+        while (S_AXIS_TREADY == 0) @(negedge S_AXIS_ACLK);
+        S_AXIS_TDATA = 10;
+        @(negedge S_AXIS_ACLK);
+        while (S_AXIS_TREADY == 0) @(negedge S_AXIS_ACLK);
+        S_AXIS_TDATA = 11;
+        @(negedge S_AXIS_ACLK);
+        while (S_AXIS_TREADY == 0) @(negedge S_AXIS_ACLK);
+        S_AXIS_TDATA = 12;
+        @(negedge S_AXIS_ACLK);
+        while (S_AXIS_TREADY == 0) @(negedge S_AXIS_ACLK);
+        S_AXIS_TDATA = 13;
+        @(negedge S_AXIS_ACLK);
+        while (S_AXIS_TREADY == 0) @(negedge S_AXIS_ACLK);
+        S_AXIS_TDATA = 14;
+        @(negedge S_AXIS_ACLK);
+        while (S_AXIS_TREADY == 0) @(negedge S_AXIS_ACLK);
+        S_AXIS_TDATA = 15;
+        @(negedge S_AXIS_ACLK);
+        while (S_AXIS_TREADY == 0) @(negedge S_AXIS_ACLK);
+        S_AXIS_TDATA = 16;
+        S_AXIS_TLAST = 1;
+        @(negedge S_AXIS_ACLK);
+        S_AXIS_TVALID = 0;
+        S_AXIS_TLAST = 0;
+
+        #100;
+
+
+        bram_sel = 1;
+        row_width = 5;
+        
+        #1;
+        
+        @(negedge S_AXIS_ACLK);
+        S_AXIS_TVALID = 1;
+        S_AXIS_TLAST = 0;
+        S_AXIS_TSTRB = 15;
+        S_AXIS_TDATA = 1;
+        @(negedge S_AXIS_ACLK);
+        while (S_AXIS_TREADY == 0) @(negedge S_AXIS_ACLK);
+        S_AXIS_TDATA = 2;
+        @(negedge S_AXIS_ACLK);
+        while (S_AXIS_TREADY == 0) @(negedge S_AXIS_ACLK);
+        S_AXIS_TDATA = 3;
+        @(negedge S_AXIS_ACLK);
+        while (S_AXIS_TREADY == 0) @(negedge S_AXIS_ACLK);
+        S_AXIS_TDATA = 4;
+        @(negedge S_AXIS_ACLK);
+        while (S_AXIS_TREADY == 0) @(negedge S_AXIS_ACLK);
+        S_AXIS_TDATA = 5;
+        @(negedge S_AXIS_ACLK);
+        while (S_AXIS_TREADY == 0) @(negedge S_AXIS_ACLK);
+        S_AXIS_TDATA = 6;
+        @(negedge S_AXIS_ACLK);
+        while (S_AXIS_TREADY == 0) @(negedge S_AXIS_ACLK);
+        S_AXIS_TDATA = 7;
+        @(negedge S_AXIS_ACLK);
+        while (S_AXIS_TREADY == 0) @(negedge S_AXIS_ACLK);
+        S_AXIS_TDATA = 8;
+        @(negedge S_AXIS_ACLK);
+        while (S_AXIS_TREADY == 0) @(negedge S_AXIS_ACLK);
+        S_AXIS_TDATA = 9;
+        @(negedge S_AXIS_ACLK);
+        while (S_AXIS_TREADY == 0) @(negedge S_AXIS_ACLK);
+        S_AXIS_TDATA = 10;
+        @(negedge S_AXIS_ACLK);
+        while (S_AXIS_TREADY == 0) @(negedge S_AXIS_ACLK);
+        S_AXIS_TDATA = 11;
+        @(negedge S_AXIS_ACLK);
+        while (S_AXIS_TREADY == 0) @(negedge S_AXIS_ACLK);
+        S_AXIS_TDATA = 12;
+        @(negedge S_AXIS_ACLK);
+        while (S_AXIS_TREADY == 0) @(negedge S_AXIS_ACLK);
+        S_AXIS_TDATA = 13;
+        @(negedge S_AXIS_ACLK);
+        while (S_AXIS_TREADY == 0) @(negedge S_AXIS_ACLK);
+        S_AXIS_TDATA = 14;
+        @(negedge S_AXIS_ACLK);
+        while (S_AXIS_TREADY == 0) @(negedge S_AXIS_ACLK);
+        S_AXIS_TDATA = 15;
+        S_AXIS_TLAST = 1;
+        @(negedge S_AXIS_ACLK);
+        S_AXIS_TVALID = 0;
+        S_AXIS_TLAST = 0;
+
+        #100;
+
+
+        bram_sel = 2;
+        row_width = 5;
+        
+        #1;
+        
+        @(negedge S_AXIS_ACLK);
+        S_AXIS_TVALID = 1;
+        S_AXIS_TLAST = 0;
+        S_AXIS_TSTRB = 15;
+        S_AXIS_TDATA = 1;
+        @(negedge S_AXIS_ACLK);
+        while (S_AXIS_TREADY == 0) @(negedge S_AXIS_ACLK);
+        S_AXIS_TDATA = 2;
+        @(negedge S_AXIS_ACLK);
+        while (S_AXIS_TREADY == 0) @(negedge S_AXIS_ACLK);
+        S_AXIS_TDATA = 3;
+        @(negedge S_AXIS_ACLK);
+        while (S_AXIS_TREADY == 0) @(negedge S_AXIS_ACLK);
+        S_AXIS_TDATA = 4;
+        @(negedge S_AXIS_ACLK);
+        while (S_AXIS_TREADY == 0) @(negedge S_AXIS_ACLK);
+        S_AXIS_TDATA = 5;
+        @(negedge S_AXIS_ACLK);
+        while (S_AXIS_TREADY == 0) @(negedge S_AXIS_ACLK);
+        S_AXIS_TDATA = 6;
+        @(negedge S_AXIS_ACLK);
+        while (S_AXIS_TREADY == 0) @(negedge S_AXIS_ACLK);
+        S_AXIS_TDATA = 7;
+        @(negedge S_AXIS_ACLK);
+        while (S_AXIS_TREADY == 0) @(negedge S_AXIS_ACLK);
+        S_AXIS_TDATA = 8;
+        @(negedge S_AXIS_ACLK);
+        while (S_AXIS_TREADY == 0) @(negedge S_AXIS_ACLK);
+        S_AXIS_TDATA = 9;
+        @(negedge S_AXIS_ACLK);
+        while (S_AXIS_TREADY == 0) @(negedge S_AXIS_ACLK);
+        S_AXIS_TDATA = 10;
+        @(negedge S_AXIS_ACLK);
+        while (S_AXIS_TREADY == 0) @(negedge S_AXIS_ACLK);
+        S_AXIS_TDATA = 11;
+        @(negedge S_AXIS_ACLK);
+        while (S_AXIS_TREADY == 0) @(negedge S_AXIS_ACLK);
+        S_AXIS_TDATA = 12;
+        @(negedge S_AXIS_ACLK);
+        while (S_AXIS_TREADY == 0) @(negedge S_AXIS_ACLK);
+        S_AXIS_TDATA = 13;
+        @(negedge S_AXIS_ACLK);
+        while (S_AXIS_TREADY == 0) @(negedge S_AXIS_ACLK);
+        S_AXIS_TDATA = 14;
+        @(negedge S_AXIS_ACLK);
+        while (S_AXIS_TREADY == 0) @(negedge S_AXIS_ACLK);
+        S_AXIS_TDATA = 15;
+        S_AXIS_TLAST = 1;
+        @(negedge S_AXIS_ACLK);
+        S_AXIS_TVALID = 0;
+        S_AXIS_TLAST = 0;
+
+        #100;
+
 
     end
 
