5647eefbeb600ebf8ee09455cd10fb0b367e812a
dakshinatharindu <tharinduokd@gmail.com>
2023-12-13 22:10:31 +0530

    include bram

--- tb/fetch_unit_M00_AXIS_tb.v
+++ tb/fetch_unit_M00_AXIS_tb.v
@@ -6,8 +6,9 @@ module fetch_unit_M00_AXIS_tb;
     parameter integer C_M_START_COUNT	= 32;
 
     reg  						VALID_PE2FU;
-    reg  	[31:0] 				mat_res_dout;
+    wire  	[31:0] 				mat_res_dout;
     wire 	[BRAM_DEPTH-1:0] 	mat_res_addr;
+    wire                        mat_res_ren;
     reg [31:0] res_size;
     reg   M_AXIS_ACLK;
     reg   M_AXIS_ARESETN;
@@ -21,6 +22,7 @@ module fetch_unit_M00_AXIS_tb;
         .VALID_PE2FU(VALID_PE2FU),
         .mat_res_dout(mat_res_dout),
         .mat_res_addr(mat_res_addr),
+        .mat_res_ren(mat_res_ren),
         .res_size(res_size),
 
         .M_AXIS_ACLK(M_AXIS_ACLK),
@@ -32,13 +34,27 @@ module fetch_unit_M00_AXIS_tb;
         .M_AXIS_TREADY(M_AXIS_TREADY)
     );
 
+    bram #(
+        .DATA_WIDTH(C_M_AXIS_TDATA_WIDTH),
+        .DEPTH(256)
+    ) bram_inst (
+        .clk(M_AXIS_ACLK),
+        .addr(mat_res_addr),
+        .data_out(mat_res_dout),
+        .data_in(32'h0),
+        .we(1'b0),
+        .re(mat_res_ren)
+    );
+
+
     initial begin
         M_AXIS_ACLK = 0;
         forever #5 M_AXIS_ACLK = ~M_AXIS_ACLK;
     end
 
     initial begin
-        res_size = 16;
+        $readmemh("/home/dakshina/Projects/ADS/simd-matrix-accelerator/tb/ram.txt", bram_inst.mem);
+        res_size = 8;
 
         M_AXIS_ARESETN = 0;
         #10 M_AXIS_ARESETN = 1;
@@ -46,40 +62,29 @@ module fetch_unit_M00_AXIS_tb;
         @(negedge M_AXIS_ACLK);
         VALID_PE2FU = 0;
         M_AXIS_TREADY = 0;
-        mat_res_dout = 1;
 
         @(negedge M_AXIS_ACLK);
         VALID_PE2FU = 1;
 
-        while (M_AXIS_TVALID == 0) begin
-            @(negedge M_AXIS_ACLK);
-        end
+        @(negedge M_AXIS_ACLK);
+        @(negedge M_AXIS_ACLK);
         M_AXIS_TREADY = 1;
 
-        while (mat_res_addr != 1) begin
-            @(negedge M_AXIS_ACLK);
-        end
         @(negedge M_AXIS_ACLK);
-        mat_res_dout = 2;
-
         @(negedge M_AXIS_ACLK);
-        mat_res_dout = 3;
+        @(negedge M_AXIS_ACLK);
+        @(negedge M_AXIS_ACLK);
+        @(negedge M_AXIS_ACLK);
+        M_AXIS_TREADY = 0;
 
         @(negedge M_AXIS_ACLK);
-        mat_res_dout = 4;
-
-        @(negedge M_AXIS_ACLK);
-        mat_res_dout = 5;
-
-        @(negedge M_AXIS_ACLK);
-        mat_res_dout = 6;
-
-        @(negedge M_AXIS_ACLK);
-        mat_res_dout = 7    ;
+        M_AXIS_TREADY = 1;
 
 
 
 
+
+        #5000;
     end
 
 
